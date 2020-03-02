8ae9e8da7b2b9b75b0f69ddc15080abe2c8167a1
fflorezz <felipeflorezart@gmail.com>
2020-03-01 20:55:45 -0500

    Added edit button

--- js/views/todo-view.js
+++ js/views/todo-view.js
@@ -19,6 +19,7 @@ var app = app || {};
 		events: {
 			"click .toggle": "toggleCompleted",
 			"dblclick label": "edit",
+			"click .edit-btn": "edit",
 			"click .destroy": "clear",
 			"keypress .edit": "updateOnEnter",
 			"keydown .edit": "revertOnEscape",
@@ -49,7 +50,7 @@ var app = app || {};
 			}
 
 			this.$el.html(this.template(this.model.toJSON()));
-			//this.$el.toggleClass("completed", this.model.get("completed"));
+			this.$el.toggleClass("completed", this.model.get("completed"));
 			this.toggleVisible();
 			this.$input = this.$(".edit");
 			return this;
