bec75515e294ed69503ea6592bc212a24d24f50c
fflorezz <felipeflorezart@gmail.com>
2020-03-01 19:44:28 -0500

    Changed todo completed

--- js/views/todo-view.js
+++ js/views/todo-view.js
@@ -1,8 +1,8 @@
 /*global Backbone, jQuery, _, ENTER_KEY, ESC_KEY */
 var app = app || {};
 
-(function ($) {
-	'use strict';
+(function($) {
+	"use strict";
 
 	// Todo Item View
 	// --------------
@@ -10,33 +10,33 @@ var app = app || {};
 	// The DOM element for a todo item...
 	app.TodoView = Backbone.View.extend({
 		//... is a list tag.
-		tagName:  'li',
+		tagName: "li",
 
 		// Cache the template function for a single item.
-		template: _.template($('#item-template').html()),
+		template: _.template($("#item-template").html()),
 
 		// The DOM events specific to an item.
 		events: {
-			'click .toggle': 'toggleCompleted',
-			'dblclick label': 'edit',
-			'click .destroy': 'clear',
-			'keypress .edit': 'updateOnEnter',
-			'keydown .edit': 'revertOnEscape',
-			'blur .edit': 'close'
+			"click .toggle": "toggleCompleted",
+			"dblclick label": "edit",
+			"click .destroy": "clear",
+			"keypress .edit": "updateOnEnter",
+			"keydown .edit": "revertOnEscape",
+			"blur .edit": "close"
 		},
 
 		// The TodoView listens for changes to its model, re-rendering. Since
 		// there's a one-to-one correspondence between a **Todo** and a
 		// **TodoView** in this app, we set a direct reference on the model for
 		// convenience.
-		initialize: function () {
-			this.listenTo(this.model, 'change', this.render);
-			this.listenTo(this.model, 'destroy', this.remove);
-			this.listenTo(this.model, 'visible', this.toggleVisible);
+		initialize: function() {
+			this.listenTo(this.model, "change", this.render);
+			this.listenTo(this.model, "destroy", this.remove);
+			this.listenTo(this.model, "visible", this.toggleVisible);
 		},
 
 		// Re-render the titles of the todo item.
-		render: function () {
+		render: function() {
 			// Backbone LocalStorage is adding `id` attribute instantly after
 			// creating a model.  This causes our TodoView to render twice. Once
 			// after creating a model and once on `id` change.  We want to
@@ -49,35 +49,33 @@ var app = app || {};
 			}
 
 			this.$el.html(this.template(this.model.toJSON()));
-			this.$el.toggleClass('completed', this.model.get('completed'));
+			//this.$el.toggleClass("completed", this.model.get("completed"));
 			this.toggleVisible();
-			this.$input = this.$('.edit');
+			this.$input = this.$(".edit");
 			return this;
 		},
 
-		toggleVisible: function () {
-			this.$el.toggleClass('hidden', this.isHidden());
+		toggleVisible: function() {
+			this.$el.toggleClass("hidden", this.isHidden());
 		},
 
-		isHidden: function () {
-			return this.model.get('completed') ?
-				app.TodoFilter === 'active' :
-				app.TodoFilter === 'completed';
+		isHidden: function() {
+			return this.model.get("completed") ? app.TodoFilter === "active" : app.TodoFilter === "completed";
 		},
 
 		// Toggle the `"completed"` state of the model.
-		toggleCompleted: function () {
+		toggleCompleted: function() {
 			this.model.toggle();
 		},
 
 		// Switch this view into `"editing"` mode, displaying the input field.
-		edit: function () {
-			this.$el.addClass('editing');
+		edit: function() {
+			this.$el.addClass("editing");
 			this.$input.focus();
 		},
 
 		// Close the `"editing"` mode, saving changes to the todo.
-		close: function () {
+		close: function() {
 			var value = this.$input.val();
 			var trimmedValue = value.trim();
 
@@ -85,7 +83,7 @@ var app = app || {};
 			// longer being edited. Relying on the CSS class here has the
 			// benefit of us not having to maintain state in the DOM and the
 			// JavaScript logic.
-			if (!this.$el.hasClass('editing')) {
+			if (!this.$el.hasClass("editing")) {
 				return;
 			}
 
@@ -98,17 +96,18 @@ var app = app || {};
 					// compare untrimmed version with a trimmed one to check
 					// whether anything changed
 					// And if yes, we've to trigger change event ourselves
-					this.model.trigger('change');
+					this.model.trigger("change");
 				}
-			} else {
+			}
+			else {
 				this.clear();
 			}
 
-			this.$el.removeClass('editing');
+			this.$el.removeClass("editing");
 		},
 
 		// If you hit `enter`, we're through editing the item.
-		updateOnEnter: function (e) {
+		updateOnEnter: function(e) {
 			if (e.which === ENTER_KEY) {
 				this.close();
 			}
@@ -116,16 +115,16 @@ var app = app || {};
 
 		// If you're pressing `escape` we revert your change by simply leaving
 		// the `editing` state.
-		revertOnEscape: function (e) {
+		revertOnEscape: function(e) {
 			if (e.which === ESC_KEY) {
-				this.$el.removeClass('editing');
+				this.$el.removeClass("editing");
 				// Also reset the hidden input back to the original value.
-				this.$input.val(this.model.get('title'));
+				this.$input.val(this.model.get("title"));
 			}
 		},
 
 		// Remove the item, destroy the model from *localStorage* and delete its view.
-		clear: function () {
+		clear: function() {
 			this.model.destroy();
 		}
 	});
